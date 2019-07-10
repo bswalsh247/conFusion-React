4afa47850efe249ba529c28fc1aa6ff3e0712488
bswalsh247 <bswaslh19@gmail.com>
2019-07-09 19:50:09 -0600

    Controlled Fonts

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   // render MainComponent to index.js file. 
   render() {
     return (
+      // BrowserRouter creates specialized history object for navigating links
       <BrowserRouter>
         <div>
           <Main />
