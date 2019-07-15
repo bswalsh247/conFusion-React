764772921b1f2294a9e0f7f3bf8560a7aeddea67
bswalsh247 <bswaslh19@gmail.com>
2019-07-15 17:43:01 -0600

    Week 3 Assignment: React Forms and Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -50,6 +50,7 @@ class Main extends Component { // this creates new component
         return(
             <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
                 comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                postComment={this.props.postComment}
             />
         );
     }
