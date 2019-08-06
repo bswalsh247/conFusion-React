5de60a73681418d694754d9a5e656fe0a766412d
bswalsh247 <bswaslh19@gmail.com>
2019-08-05 19:58:47 -0600

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,9 @@ import { Card, CardImg, CardText, CardBody,Modal, ModalHeader, ModalBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Button , Row, Col, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-import { Loading } from './LoadingComponent'
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -49,7 +51,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
             return(
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
                         <CardBody>
                                 <CardTitle>{dish.name}</CardTitle>
                                 <CardText>{dish.description}</CardText>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'; 
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 // these will props will now be able to connect to Main Component
@@ -26,8 +26,9 @@ const mapStateToProps = state => {
 const mapDispatchToProps = (dispatch) => ({
     addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
     fetchDishes: () => {dispatch(fetchDishes())},
-    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
-
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos())
 });
 // A component returns a set of React elements that should appear on the screen
 // Components enable you to split your UI into independent, reusable pieces
@@ -47,6 +48,8 @@ class Main extends Component { // this creates new component
 
   componentDidMount() {
     this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
   }
 
     render() {  // any class component in react needs to implent this component 
@@ -63,7 +66,9 @@ class Main extends Component { // this creates new component
                 dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
                 dishesLoading={this.props.dishes.isLoading}
                 dishesErrMess={this.props.dishes.errMess}
-                promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                promosLoading={this.props.promotions.isLoading}
+                promosErrMess={this.props.promotions.errMess}
                 leader={this.props.leaders.filter((leader) => leader.featured)[0]}
             />
         );
@@ -74,7 +79,8 @@ class Main extends Component { // this creates new component
             <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
               isLoading={this.props.dishes.isLoading}
               errMess={this.props.dishes.errMess}
-              comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+              comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+              commentsErrMess={this.props.comments.errMess}
               addComment={this.props.addComment}
             />
         );
